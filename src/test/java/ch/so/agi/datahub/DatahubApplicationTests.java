a5f5ad50ed90b4d06f2b421d13153fa58a33d18c
edigonzales <edi.gonzales@gmail.com>
2021-06-13 19:33:16 +0200

    initial commit

---
+++ src/test/java/ch/so/agi/datahub/DatahubApplicationTests.java
@@ -0,0 +1,13 @@
+package ch.so.agi.datahub;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DatahubApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
