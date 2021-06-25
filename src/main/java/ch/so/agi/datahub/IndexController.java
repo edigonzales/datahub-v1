6dfaa0ad1c78b3fdf154d366dbf49b6eba0f3505
edigonzales <edi.gonzales@gmail.com>
2021-06-25 08:34:55 +0200

    add ping

--- src/main/java/ch/so/agi/datahub/IndexController.java
+++ src/main/java/ch/so/agi/datahub/IndexController.java
@@ -4,9 +4,17 @@ import org.springframework.security.core.Authentication;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 
 @Controller
 public class IndexController {
+    
+    @RequestMapping(value = "/ping", method = RequestMethod.GET)
+    public String ping() {
+        return "ping";
+    }
+    
 
     @GetMapping
     public String getIndexPage(Model model, Authentication authentication) {
